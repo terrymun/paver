e18016240d308c5820f0c6ff02859212aa6d61ff
Terry Mun <hello@terrymun.com>
2015-05-20 09:16:00 +0200

    Minor updates
    - Added JS code samples to usage notes
    - Reduced var declarations in normalcdf function

--- jquery.paver.js
+++ jquery.paver.js
@@ -354,15 +354,15 @@
 			//// ------------------ ////
 			// Adapted from http://stackoverflow.com/questions/5259421/cumulative-distribution-function-in-javascript
 			normalcdf: function(mean, sigma, to) {
-				var z = (to-mean)/Math.sqrt(2*sigma*sigma);
-				var t = 1/(1+0.3275911*Math.abs(z));
-				var a1 =  0.254829592;
-				var a2 = -0.284496736;
-				var a3 =  1.421413741;
-				var a4 = -1.453152027;
-				var a5 =  1.061405429;
-				var erf = 1-(((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-z*z);
-				var sign = 1;
+				var z = (to-mean)/Math.sqrt(2*sigma*sigma),
+					t = 1/(1+0.3275911*Math.abs(z)),
+					a1 =  0.254829592,
+					a2 = -0.284496736,
+					a3 =  1.421413741,
+					a4 = -1.453152027,
+					a5 =  1.061405429,
+					erf = 1-(((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*Math.exp(-z*z),
+					sign = 1;
 				if(z < 0) sign = -1;
 				return (1/2)*(1+sign*erf);
 			},
